b6d410ad0c42554c2a1a6a59a15bcb9be814c6c2
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-16 18:14:13 -0500

    Added cohort id

--- migrations/20190116164103_students.js
+++ migrations/20190116164103_students.js
@@ -2,9 +2,13 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('students', function(tbl) {
     tbl.increments('id');
     tbl.string('name', 255);
+    tbl
+      .integer('cohort_id')
+      .references('id')
+      .inTable('cohorts');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return;
+  return knex.schema.dropTable('students');
 };
--- seeds/01-students.js
+++ seeds/01-students.js
@@ -1,13 +1,11 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('students')
     .truncate()
     .then(function() {
-      // Inserts seed entries
       return knex('students').insert([
-        { name: 'steve' },
-        { name: 'stevo' },
-        { name: 'stephen' }
+        { name: 'steve', cohort_id: 1 },
+        { name: 'stevo', cohort_id: 2 },
+        { name: 'stephen', cohort_id: 3 }
       ]);
     });
 };
